a79349623c9e1c1a28a55be7ccc927946243fca2
mmedojevic <Marko.Medojevic@colorado.edu>
2025-05-20 19:37:23 +0000

    asignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
@@ -5,13 +6,14 @@
 set -e
 set -u
 
+CURRENT_DIR=$(pwd)
 OUTDIR=/tmp/aeld
 KERNEL_REPO=git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git
 KERNEL_VERSION=v5.15.163
 BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
-CROSS_COMPILE=aarch64-none-linux-gnu-
+CROSS_COMPILE=aarch64-linux-gnu-
 
 if [ $# -lt 1 ]
 then
@@ -35,10 +37,13 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} allyesconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
 fi
 
 echo "Adding the Image in outdir"
-
+cp /tmp/aeld/linux-stable/arch/arm64/boot/Image ${OUTDIR}
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
@@ -48,6 +53,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +65,59 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+make defconfig
+make ARCH=${ARCH} CROSS=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
+cp /usr/aarch64-linux-gnu/lib/libm.so.6 ${OUTDIR}/rootfs/lib
+cp /usr/aarch64-linux-gnu/lib/libresolv.so.2 ${OUTDIR}/rootfs/lib
+cp /usr/aarch64-linux-gnu/lib/libc.so.6 ${OUTDIR}/rootfs/lib
+cp /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib64
+cp /usr/aarch64-linux-gnu/lib/libm.so.6 ${OUTDIR}/rootfs/lib64
+cp /usr/aarch64-linux-gnu/lib/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+cp /usr/aarch64-linux-gnu/lib/libc.so.6 ${OUTDIR}/rootfs/lib64
+
 
 # TODO: Make device nodes
+cd ${OUTDIR}
+sudo rm -f ./dev/null
+sudo rm -f ./dev/console
+sudo mknod ./dev/null c 1 3
+sudo chmod 666 ./dev/null
+sudo mknod ./dev/console c 5 1
+sudo chmod 600 ./dev/console
+sudo chown root:tty ./dev/console
+
 
 # TODO: Clean and build the writer utility
+cd $CURRENT_DIR
+make clean
+make
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ./writer ${OUTDIR}/rootfs/home/
+cp ./finder.sh ${OUTDIR}/rootfs/home/
+cp ./finder-test.sh ${OUTDIR}/rootfs/home/
+cp ./writer.sh ${OUTDIR}/rootfs/home/
+cp ./autorun-qemu.sh ${OUTDIR}/rootfs/home/
+mkdir ${OUTDIR}/rootfs/home/conf
+cp ./conf/assignment.txt ${OUTDIR}/rootfs/home/conf/
+cp ./conf/username.txt ${OUTDIR}/rootfs/home/conf/
+ 
 
 # TODO: Chown the root directory
+sudo chown -R marko:marko ${OUTDIR}
 
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner marko:marko > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
